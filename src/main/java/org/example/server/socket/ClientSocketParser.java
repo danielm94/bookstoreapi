12eb97ff181c8607bd33203bab94d26033facefa
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-19 21:14:22 -0500

    Add socket to HttpExchange method to ClientSocketParser

--- src/main/java/org/example/server/socket/ClientSocketParser.java
+++ src/main/java/org/example/server/socket/ClientSocketParser.java
@@ -1,42 +1,95 @@
 package org.example.server.socket;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpContext;
+import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.val;
+import org.example.server.HttpMethod;
 import org.example.server.exchange.BookHttpExchange;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 
 public class ClientSocketParser {
+    public static final int NO_BODY_IN_REQUEST = -1;
     private final Map<String, HttpContext> contextMap;
 
-    public ClientSocketParser(Map<String, HttpContext> contextMap) {
+    public ClientSocketParser(@NonNull Map<String, HttpContext> contextMap) {
         this.contextMap = contextMap;
     }
 
     @SneakyThrows(IOException.class)
-    public static BookHttpExchange getBookHttpExchangeFromClientSocket(Socket clientSocket) {
+    public BookHttpExchange getBookHttpExchangeFromClientSocket(@NonNull Socket clientSocket) {
         val inputStream = clientSocket.getInputStream();
         val scanner = new Scanner(inputStream);
+        val requestList = new ArrayList<String>();
+        while (scanner.hasNext()) {
+            requestList.add(scanner.nextLine());
+        }
 
-        return null;
+        val exchange = new BookHttpExchange();
+        parseRequestLine(requestList, exchange);
+
+        val messageBodyLine = parseHeaders(requestList, exchange);
+
+        if (messageBodyLine != NO_BODY_IN_REQUEST) {
+            parseBody(requestList, exchange, messageBodyLine + 1);
+        }
+        return exchange;
     }
-//    clientSocketText = {
-//        ArrayList @923}  size = 12
-//            0 = "GET / HTTP/1.1"
-//            1 = "Content-Type: application/json"
-//            2 = "User-Agent: PostmanRuntime/7.36.0"
-//            3 = "Accept: */*"
-//            4 = "Cache-Control: no-cache"
-//            5 = "Postman-Token: c60dfa73-b015-46d4-b9be-89099876e138"
-//            6 = "Host: localhost:8000"
-//            7 = "Accept-Encoding: gzip, deflate, br"
-//            8 = "Connection: keep-alive"
-//            9 = "Content-Length: 19"
-//            10 = ""
-//            11 = "Hello from Postman!"
+
+    private void parseBody(@NonNull ArrayList<String> requestList, @NonNull BookHttpExchange exchange, int start) {
+        val bodyBuilder = new StringBuilder();
+        for (var i = start; i < requestList.size(); i++) {
+            bodyBuilder.append(requestList.get(i)).append(System.lineSeparator());
+        }
+        val body = bodyBuilder.toString();
+        val bodyStream = new ByteArrayInputStream(body.getBytes());
+        exchange.setRequestBody(bodyStream);
+    }
+
+    private void parseRequestLine(@NonNull List<String> requestList, @NonNull BookHttpExchange exchange) {
+        val requestLine = requestList.getFirst();
+        val requestLineValues = requestLine.split("\\s");
+        if (requestLineValues.length < 3) exchange.setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST);
+        val requestMethod = requestLineValues[0];
+        validateRequestMethod(exchange, requestMethod);
+        exchange.setRequestMethod(requestMethod);
+        val path = requestLineValues[1];
+        val context = contextMap.get(path);
+        if (context == null) exchange.setResponseCode(HttpURLConnection.HTTP_NOT_FOUND);
+        exchange.setHttpContext(context);
+        val protocol = requestLineValues[2];
+        exchange.setProtocol(protocol);
+    }
+
+    private int parseHeaders(@NonNull List<String> requestList, @NonNull BookHttpExchange exchange) {
+        val headers = new Headers();
+        exchange.setRequestHeaders(headers);
+        for (var i = 1; i < requestList.size(); i++) {
+            var line = requestList.get(i);
+            var endOfHeadersReached = line.isBlank() || line.isEmpty();
+            if (endOfHeadersReached) return i;
+            var keyValueArray = requestList.get(i).split(":");
+            headers.add(keyValueArray[0], keyValueArray[1]);
+        }
+
+        return NO_BODY_IN_REQUEST;
+    }
+
+    private static void validateRequestMethod(@NonNull BookHttpExchange exchange, @NonNull String requestMethod) {
+        try {
+            HttpMethod.valueOf(requestMethod);
+        } catch (IllegalArgumentException e) {
+            exchange.setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST);
+        }
+    }
 }
