43b9efb28c49ecdea3921fe9b35540c953564581
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-20 20:03:23 -0500

    Initial commit of FailureHandler

---
+++ src/main/java/org/example/server/handlers/FailureHandler.java
@@ -0,0 +1,31 @@
+package org.example.server.handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import lombok.AllArgsConstructor;
+import lombok.extern.flogger.Flogger;
+import lombok.val;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Flogger
+@AllArgsConstructor
+public class FailureHandler implements HttpHandler {
+    private final int statusCode;
+    private final String message;
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        val responseBytes = message.getBytes(StandardCharsets.UTF_8);
+        exchange.sendResponseHeaders(statusCode, responseBytes.length);
+        try (val body = exchange.getResponseBody()) {
+            body.write(responseBytes);
+        } catch (IOException e) {
+            log.atSevere()
+               .withCause(e)
+               .log("Failed to write response to the client.",
+                       statusCode, message, exchange);
+        }
+    }
+}
