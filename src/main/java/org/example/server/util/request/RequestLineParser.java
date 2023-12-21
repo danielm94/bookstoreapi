0a14140d4bc90e922501a404d2117e8dca8cf987
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-20 19:01:17 -0500

    Initial commit of RequestLineParser

---
+++ src/main/java/org/example/server/util/request/RequestLineParser.java
@@ -0,0 +1,52 @@
+package org.example.server.util.request;
+
+import lombok.extern.flogger.Flogger;
+import lombok.val;
+import org.apache.commons.lang3.StringUtils;
+import org.example.server.HttpMethod;
+
+@Flogger
+public class RequestLineParser {
+    public static final String SPLIT_USING_WHITESPACE_REGEX_PATTERN = "\\s";
+    public static final int NUMBER_OF_COMPONENTS_IN_REQUEST_LINE = 3;
+
+    private RequestLineParser() {
+    }
+
+    public static RequestLine parseRequestLine(String requestLineString) {
+        if (StringUtils.isBlank(requestLineString)) {
+            log.atWarning()
+               .log("Could not parse request line as it was null/empty/blank.");
+            return null;
+        }
+        val requestLine = new RequestLine();
+
+        val requestLineValues = requestLineString.split(SPLIT_USING_WHITESPACE_REGEX_PATTERN);
+
+        if (requestLineValues.length < NUMBER_OF_COMPONENTS_IN_REQUEST_LINE) {
+            log.atWarning()
+               .log("The request line sent by the client did not include all %s expected components. " +
+                       "Request line: %s", NUMBER_OF_COMPONENTS_IN_REQUEST_LINE, requestLineString);
+            return null;
+        }
+
+        val requestMethodString = requestLineValues[0];
+        HttpMethod requestMethod = null;
+        try {
+            requestMethod = HttpMethod.valueOf(requestMethodString);
+        } catch (IllegalArgumentException e) {
+            log.atWarning()
+               .log("Client sent an unsupported request method - [%s]", requestMethodString);
+        }
+        requestLine.setHttpMethod(requestMethod);
+
+        val path = requestLineValues[1];
+        requestLine.setPath(path);
+
+        val protocol = requestLineValues[2];
+        requestLine.setProtocol(protocol);
+
+        return requestLine;
+    }
+
+}
