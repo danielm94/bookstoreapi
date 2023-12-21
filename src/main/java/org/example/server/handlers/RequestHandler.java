7dd473ce3d724711c538c648a5ae28bd139aeed7
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-20 19:06:14 -0500

    Refactor RequestHandler

--- src/main/java/org/example/server/handlers/RequestHandler.java
+++ src/main/java/org/example/server/handlers/RequestHandler.java
@@ -2,22 +2,26 @@ package org.example.server.handlers;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import lombok.val;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class RequestHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        var inputStream = exchange.getRequestBody();
-        var scanner = new Scanner(inputStream);
-        var data = scanner.nextLine();
-        System.out.println(data);
-        var message = "Hey I got your message! You sent me the following body - " + data;
-        exchange.sendResponseHeaders(200, message.length());
-        var body = exchange.getResponseBody();
-        body.write(message.getBytes());
+        val requestBody = exchange.getRequestBody();
+        val data = requestBody == null ? "" : new Scanner(exchange.getRequestBody()).useDelimiter("\\A").next();
+
+        val message = "Hey I got your message! You sent me the following body - " + data;
+        val responseBytes = message.getBytes(StandardCharsets.UTF_8);
+
+        exchange.sendResponseHeaders(200, responseBytes.length);
+        val body = exchange.getResponseBody();
+        body.write(responseBytes);
         body.close();
     }
+
 
 }
