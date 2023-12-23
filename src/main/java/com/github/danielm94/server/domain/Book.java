f17a3a137990c1a466d743b63bba29745710d59c
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-23 04:29:39 -0500

    Make isbn string, add builder and jacksonized annotations.

--- src/main/java/com/github/danielm94/server/domain/Book.java
+++ src/main/java/com/github/danielm94/server/domain/Book.java
@@ -1,17 +1,21 @@
-package com.github.danielm94;
+package com.github.danielm94.server.domain;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Builder
+@Jacksonized
 public class Book {
     private final UUID id;
     private String bookName;
     private String author;
-    private long isbn;
+    private String isbn;
     private BigDecimal price;
     private LocalDateTime dateAdded;
     private LocalDateTime dateUpdated;
