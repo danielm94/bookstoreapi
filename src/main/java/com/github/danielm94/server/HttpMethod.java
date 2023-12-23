74bdbae5dbb791140e52cf344b04f43fa3f9dae0
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-23 04:30:42 -0500

    Add static hash map to get http method enums fast

--- src/main/java/com/github/danielm94/server/HttpMethod.java
+++ src/main/java/com/github/danielm94/server/HttpMethod.java
@@ -1,5 +1,10 @@
 package com.github.danielm94.server;
 
+import lombok.val;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public enum HttpMethod {
     GET,
     HEAD,
@@ -9,5 +14,19 @@ public enum HttpMethod {
     CONNECT,
     OPTIONS,
     TRACE,
-    PATCH
+    PATCH;
+
+    private static final Map<String, HttpMethod> nameMethodMap;
+
+    static {
+        nameMethodMap = new HashMap<>();
+        for (val value : HttpMethod.values()) {
+            nameMethodMap.put(value.toString().toUpperCase(), value);
+        }
+    }
+
+
+    public static HttpMethod getHttpMethodFromStringValue(String httpMethodString) {
+        return nameMethodMap.get(httpMethodString.toUpperCase());
+    }
 }
