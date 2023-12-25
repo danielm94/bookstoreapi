c8458c0a668ea5a1cc53a9b15ce5b55b0e817558
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-25 17:45:56 -0500

    Initial commit of DatabaseTables.java

---
+++ src/main/java/com/github/danielm94/database/schemas/bookstoreapi/DatabaseTables.java
@@ -0,0 +1,14 @@
+package com.github.danielm94.database.schemas.bookstoreapi;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum DatabaseTables {
+    BOOKS("books");
+
+    private final String tableName;
+
+
+}
