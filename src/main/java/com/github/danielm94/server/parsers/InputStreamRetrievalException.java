16839ed69c77721c2dd26d68392bd5ac4dcf2c4f
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-26 17:37:37 -0500

    Initial commit of InputStreamRetrievalException

---
+++ src/main/java/com/github/danielm94/server/parsers/InputStreamRetrievalException.java
@@ -0,0 +1,7 @@
+package com.github.danielm94.server.parsers;
+
+public class InputStreamRetrievalException extends Exception {
+    public InputStreamRetrievalException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
