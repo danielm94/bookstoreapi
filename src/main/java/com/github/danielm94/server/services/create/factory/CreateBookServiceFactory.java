2aa99a878a3fc8ff59ade182dd3bcef93f4e2f36
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-28 15:36:22 -0500

    Fix imports.

--- src/main/java/com/github/danielm94/server/services/create/factory/CreateBookServiceFactory.java
+++ src/main/java/com/github/danielm94/server/services/create/factory/CreateBookServiceFactory.java
@@ -2,6 +2,7 @@ package com.github.danielm94.server.services.create.factory;
 
 import com.github.danielm94.server.requestdata.content.ContentType;
 import com.github.danielm94.server.services.create.CreateBookService;
+import com.github.danielm94.server.services.exceptions.UnsupportedContentTypeException;
 import lombok.NonNull;
 
 public interface CreateBookServiceFactory {
