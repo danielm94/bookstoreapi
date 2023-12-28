71fc0a064ae97c7b34d6b880d9d7b1addb6ded46
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-28 15:35:06 -0500

    Moved exception to its own package.

--- src/main/java/com/github/danielm94/server/services/exceptions/UnsupportedContentTypeException.java
+++ src/main/java/com/github/danielm94/server/services/exceptions/UnsupportedContentTypeException.java
@@ -1,4 +1,4 @@
-package com.github.danielm94.server.services.create.factory;
+package com.github.danielm94.server.services.exceptions;
 
 public class UnsupportedContentTypeException extends Exception {
     public UnsupportedContentTypeException(String message) {
