fe69c78fa660ad9405489c18cb93d47f89b44e00
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-28 03:17:03 -0500

    Initial commit of ContentTypeCreateBookServiceFactory

---
+++ src/main/java/com/github/danielm94/server/services/create/factory/ContentTypeCreateBookServiceFactory.java
@@ -0,0 +1,22 @@
+package com.github.danielm94.server.services.create.factory;
+
+import com.github.danielm94.server.requestdata.content.ContentType;
+import com.github.danielm94.server.services.create.CreateBookService;
+import com.github.danielm94.server.services.create.JsonCreateBookService;
+import lombok.NonNull;
+import lombok.val;
+
+public class ContentTypeCreateBookServiceFactory implements CreateBookServiceFactory {
+    @Override
+    public CreateBookService getServiceForContentType(@NonNull ContentType contentType) throws UnsupportedContentTypeException {
+        switch (contentType) {
+            case APPLICATION_JSON -> {
+                return new JsonCreateBookService();
+            }
+            default -> {
+                val exceptionMessage = "Server currently doesn't support creating books using content type: " + contentType;
+                throw new UnsupportedContentTypeException(exceptionMessage);
+            }
+        }
+    }
+}
