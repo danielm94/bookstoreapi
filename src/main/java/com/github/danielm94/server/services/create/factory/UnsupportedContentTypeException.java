413ece979262457658c9a833d0ef97aa9cb3470f
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-28 03:20:58 -0500

    Initial commit of UnsupportedContentTypeException.java

---
+++ src/main/java/com/github/danielm94/server/services/create/factory/UnsupportedContentTypeException.java
@@ -0,0 +1,7 @@
+package com.github.danielm94.server.services.create.factory;
+
+public class UnsupportedContentTypeException extends Exception {
+    public UnsupportedContentTypeException(String message) {
+        super(message);
+    }
+}
