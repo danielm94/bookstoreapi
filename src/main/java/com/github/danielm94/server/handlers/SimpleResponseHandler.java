bdefb94f859eea5e59fbeccfeb8fe20d826d6068
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-28 06:08:27 -0500

    handle() in SimpleResponseHandler uses ResponseDispatcher now.

--- src/main/java/com/github/danielm94/server/handlers/SimpleResponseHandler.java
+++ src/main/java/com/github/danielm94/server/handlers/SimpleResponseHandler.java
@@ -1,31 +1,24 @@
 package com.github.danielm94.server.handlers;
 
+import com.github.danielm94.server.response.ResponseDispatcher;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import lombok.AllArgsConstructor;
 import lombok.extern.flogger.Flogger;
-import lombok.val;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 @Flogger
 @AllArgsConstructor
 public class SimpleResponseHandler implements HttpHandler {
     private final int statusCode;
-    private final String message;
+    private final String body;
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        val responseBytes = message.getBytes(StandardCharsets.UTF_8);
-        exchange.sendResponseHeaders(statusCode, responseBytes.length);
-        try (val body = exchange.getResponseBody()) {
-            body.write(responseBytes);
-        } catch (IOException e) {
-            log.atSevere()
-               .withCause(e)
-               .log("Failed to write response to the client.",
-                       statusCode, message, exchange);
-        }
+        ResponseDispatcher.createResponse(exchange)
+                          .setResponseCode(statusCode)
+                          .setBody(body)
+                          .sendResponse();
     }
 }
