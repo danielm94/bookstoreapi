65e6afa0a6e2dd11fe91e2784dae3ce6a2d0f000
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-27 23:24:00 -0500

    Add AllArgsConstructor lombok annotation to DatabaseSchemas.

--- src/main/java/com/github/danielm94/database/schemas/DatabaseSchemas.java
+++ src/main/java/com/github/danielm94/database/schemas/DatabaseSchemas.java
@@ -1,16 +1,14 @@
 package com.github.danielm94.database.schemas;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum DatabaseSchemas {
     BOOKSTOREAPI("bookstoreapi");
 
     private final String schemaName;
-
-    DatabaseSchemas(String schemaName) {
-        this.schemaName = schemaName;
-    }
 
     @Override
     public String toString() {
