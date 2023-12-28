d07723b43a2981b61fa594d84c8fd2d9f8ba2c86
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-28 06:11:47 -0500

    Refactor BookHandler such that it is capable of handling different content types in the future.

--- src/main/java/com/github/danielm94/server/handlers/BookHandler.java
+++ src/main/java/com/github/danielm94/server/handlers/BookHandler.java
@@ -1,19 +1,37 @@
 package com.github.danielm94.server.handlers;
 
 import com.github.danielm94.server.HttpMethod;
-import com.github.danielm94.server.services.create.JsonCreateBookService;
+import com.github.danielm94.server.requestdata.content.ContentType;
+import com.github.danielm94.server.requestdata.headers.HttpHeader;
+import com.github.danielm94.server.services.create.CreateBookService;
+import com.github.danielm94.server.services.create.factory.ContentTypeCreateBookServiceFactory;
+import com.github.danielm94.server.services.create.factory.UnsupportedContentTypeException;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import lombok.SneakyThrows;
 import lombok.val;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
 public class BookHandler implements HttpHandler {
-    @SneakyThrows
+
     @Override
-    public void handle(HttpExchange exchange) {
+    public void handle(HttpExchange exchange) throws IOException {
         val httpMethod = HttpMethod.getHttpMethodFromStringValue(exchange.getRequestMethod());
+        val headers = exchange.getRequestHeaders();
         if (httpMethod == HttpMethod.POST) {
-            val createBookService = new JsonCreateBookService();
+            val contentTypeHeaderValue = headers.getFirst(HttpHeader.CONTENT_TYPE.toString());
+            val contentType = ContentType.getContentTypeFromString(contentTypeHeaderValue);
+            val serviceFactory = new ContentTypeCreateBookServiceFactory();
+            CreateBookService createBookService;
+            try {
+                createBookService = serviceFactory.getServiceForContentType(contentType);
+            } catch (UnsupportedContentTypeException e) {
+                val responseMessage = String.format("Server does not support content type [%s] for http method [%s] at the endpoint [%s].",
+                        contentType, httpMethod, exchange.getHttpContext().getPath());
+                new SimpleResponseHandler(HttpURLConnection.HTTP_UNSUPPORTED_TYPE, responseMessage).handle(exchange);
+                return;
+            }
             createBookService.createBook(exchange);
         }
     }
