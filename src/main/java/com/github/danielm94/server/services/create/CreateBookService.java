371ba548cb261d571621aba087b1d056d8aedcb0
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-27 23:55:24 -0500

    Add non null annotation to exchange.

--- src/main/java/com/github/danielm94/server/services/create/CreateBookService.java
+++ src/main/java/com/github/danielm94/server/services/create/CreateBookService.java
@@ -1,8 +1,9 @@
 package com.github.danielm94.server.services.create;
 
 import com.sun.net.httpserver.HttpExchange;
+import lombok.NonNull;
 
 public interface CreateBookService {
 
-    void createBook(HttpExchange exchange);
+    void createBook(@NonNull HttpExchange exchange);
 }
