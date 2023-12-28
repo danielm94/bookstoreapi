45a4a7b259061c203b487e13c562f8637d7b928c
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-28 15:21:25 -0500

    Write implementation for GetBookByIdService

--- src/main/java/com/github/danielm94/server/services/read/GetBookByIdService.java
+++ src/main/java/com/github/danielm94/server/services/read/GetBookByIdService.java
@@ -1,11 +1,94 @@
 package com.github.danielm94.server.services.read;
 
+import com.github.danielm94.server.domain.book.Book;
+import com.github.danielm94.server.domain.book.serializer.BookSerializationException;
+import com.github.danielm94.server.domain.book.serializer.JsonBookSerializer;
+import com.github.danielm94.server.handlers.SimpleResponseHandler;
 import com.sun.net.httpserver.HttpExchange;
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
+import lombok.extern.flogger.Flogger;
+import lombok.val;
 
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.UUID;
+
+import static com.github.danielm94.database.repository.BookRepository.*;
+import static com.github.danielm94.database.schemas.DatabaseSchemas.*;
+import static com.github.danielm94.database.schemas.bookstoreapi.DatabaseTables.*;
+import static com.github.danielm94.server.domain.book.mappers.BookMapper.*;
+import static com.github.danielm94.server.requestdata.content.ContentType.*;
+import static com.github.danielm94.server.requestdata.headers.HttpHeader.*;
+import static com.github.danielm94.server.response.ResponseDispatcher.createResponse;
+import static java.net.HttpURLConnection.*;
+
+@Flogger
+@AllArgsConstructor
 public class GetBookByIdService implements GetBookService {
-    @Override
-    public void getBook(@NonNull HttpExchange exchange) {
+    private final UUID uuid;
 
+    @Override
+    public void get(@NonNull HttpExchange exchange) {
+        ResultSet resultSet;
+        try {
+            resultSet = getBook(uuid);
+        } catch (SQLException e) {
+            sendResponseToClient(exchange, HTTP_INTERNAL_ERROR, "An error occurred while getting a book from the database.");
+            return;
+        } catch (InterruptedException e) {
+            sendResponseToClient(exchange, HTTP_INTERNAL_ERROR, "An error occurred while managing connections to the database.");
+            return;
+        }
+
+        List<Book> books;
+        try {
+            books = mapFromResultSet(resultSet);
+        } catch (SQLException e) {
+            sendResponseToClient(exchange, HTTP_INTERNAL_ERROR, "An error occurred while mapping data from the database.");
+            return;
+        }
+
+        if (books.size() > 1) {
+            val exceptionMessage = String.format("Duplicate entries detected in the %s.%s table.", BOOKSTOREAPI, BOOKS);
+            throw new IllegalStateException(exceptionMessage);
+        } else if (books.isEmpty()) {
+            sendResponseToClient(exchange, HTTP_NOT_FOUND, "There are no books with a UUID of: " + uuid);
+            return;
+        }
+
+        val book = books.getFirst();
+        val serializer = new JsonBookSerializer();
+        String body;
+        try {
+            body = serializer.serializeBook(book);
+        } catch (BookSerializationException e) {
+            sendResponseToClient(exchange, HTTP_INTERNAL_ERROR, "Server failed to create response body.");
+            return;
+        }
+
+        try {
+            createResponse(exchange)
+                    .setResponseCode(HTTP_OK)
+                    .addHeader(CONTENT_TYPE.toString(), APPLICATION_JSON.toString())
+                    .setBody(body)
+                    .sendResponse();
+        } catch (IOException e) {
+            log.atSevere()
+               .withCause(e)
+               .log("Failed to send response to server after getting book by id.");
+        }
+    }
+
+    private void sendResponseToClient(HttpExchange exchange, int responseStatus, String message) {
+        try {
+            new SimpleResponseHandler(responseStatus, message).handle(exchange);
+        } catch (IOException e) {
+            log.atSevere()
+               .withCause(e)
+               .log("Server failed to send response to the client.\nResponse Status: %d\nResponse Message: %s", responseStatus, message);
+        }
     }
 }
