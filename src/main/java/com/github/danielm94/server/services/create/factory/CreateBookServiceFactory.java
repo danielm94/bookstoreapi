a317cd291210bed27eabd9bb0e3ff63ee23c302a
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-28 03:16:50 -0500

    Initial commit of CreateBookServiceFactory

---
+++ src/main/java/com/github/danielm94/server/services/create/factory/CreateBookServiceFactory.java
@@ -0,0 +1,9 @@
+package com.github.danielm94.server.services.create.factory;
+
+import com.github.danielm94.server.requestdata.content.ContentType;
+import com.github.danielm94.server.services.create.CreateBookService;
+import lombok.NonNull;
+
+public interface CreateBookServiceFactory {
+    CreateBookService getServiceForContentType(@NonNull ContentType contentType) throws UnsupportedContentTypeException;
+}
