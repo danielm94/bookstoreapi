61e0d25b21cd719dec445cb9e95e6cb8fdea997c
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-27 23:25:06 -0500

    Replace lombok toString annotation with toString method.

--- src/main/java/com/github/danielm94/server/requestdata/content/ContentType.java
+++ src/main/java/com/github/danielm94/server/requestdata/content/ContentType.java
@@ -2,13 +2,16 @@ package com.github.danielm94.server.requestdata.content;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
-@ToString(of = "mimeType")
 public enum ContentType {
     APPLICATION_JSON("application/json");
 
     private final String mimeType;
+
+    @Override
+    public String toString() {
+        return mimeType;
+    }
 }
