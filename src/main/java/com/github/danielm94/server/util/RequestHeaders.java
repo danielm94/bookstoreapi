992e3a8926dcf885eb3ec0f84e26903f2454caff
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-27 22:36:14 -0500

    Add request header for content type, add to String method.

--- src/main/java/com/github/danielm94/server/util/RequestHeaders.java
+++ src/main/java/com/github/danielm94/server/util/RequestHeaders.java
@@ -6,7 +6,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum RequestHeaders {
-    CONTENT_LENGTH("Content-Length");
+    CONTENT_LENGTH("Content-Length"),
+    CONTENT_TYPE("Content-Type");
 
     private final String headerKey;
+
+    @Override
+    public String toString() {
+        return headerKey;
     }
+}
