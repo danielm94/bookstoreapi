141917595bdcbd254235ffed3990cb2a58eb4c9d
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-28 14:54:18 -0500

    Add methods to get books, clean up code.

--- src/main/java/com/github/danielm94/database/repository/BookRepository.java
+++ src/main/java/com/github/danielm94/database/repository/BookRepository.java
@@ -1,37 +1,76 @@
 package com.github.danielm94.database.repository;
 
-import com.github.danielm94.ConnectionPoolManager;
 import com.github.danielm94.database.schemas.DatabaseSchemas;
 import com.github.danielm94.database.schemas.bookstoreapi.DatabaseTables;
 import com.github.danielm94.server.domain.book.Book;
+import lombok.NonNull;
 import lombok.extern.flogger.Flogger;
 import lombok.val;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.UUID;
 
+import static com.github.danielm94.ConnectionPoolManager.getInstance;
+import static com.github.danielm94.database.schemas.DatabaseSchemas.*;
+import static com.github.danielm94.database.schemas.bookstoreapi.DatabaseTables.*;
 import static com.github.danielm94.database.schemas.bookstoreapi.books.BooksColumn.*;
 
 @Flogger
 public class BookRepository {
-    public static final DatabaseSchemas SCHEMA = DatabaseSchemas.BOOKSTOREAPI;
-    public static final DatabaseTables TABLE = DatabaseTables.BOOKS;
+    public static final DatabaseSchemas SCHEMA = BOOKSTOREAPI;
+    public static final DatabaseTables TABLE = BOOKS;
+    public static final String SELECT_BOOK_BY_ID_FORMATTABLE_QUERY = "select * from %s.%s where %s = '%s';";
+    public static final String CREATE_NEW_BOOK_FORMATTABLE_QUERY = "insert into %s.%s(%s,%s,%s,%s,%s,%s,%s) values('%s','%s','%s','%s','%s','%s','%s');";
 
     private BookRepository() {
     }
 
-    public static boolean createBook(Book book) throws SQLException, InterruptedException {
+    public static boolean createBook(@NonNull Book book) throws SQLException, InterruptedException {
         log.atFine().log("Creating book in %s.%s table for the book:\n%s", SCHEMA, TABLE, book);
-        val connection = ConnectionPoolManager.getInstance().getConnection();
+        val connection = getInstance().getConnection();
 
         val statement = connection.createStatement();
-        val query = String.format("insert into %s.%s(%s,%s,%s,%s,%s,%s,%s) values('%s','%s','%s','%s','%s','%s','%s')",
+        val query = String.format(CREATE_NEW_BOOK_FORMATTABLE_QUERY,
                 SCHEMA, TABLE,
                 ID, BOOK_NAME, AUTHOR, ISBN, PRICE, DATE_ADDED, DATE_UPDATED,
                 book.getId(), book.getBookName(), book.getAuthor(), book.getIsbn(), book.getPrice(), book.getDateAdded(), book.getDateUpdated());
+
         log.atFine().log("Executing query: %s", query);
         val rowsAffected = statement.executeUpdate(query);
         log.atFine().log("Query has finished executing. Rows affected: %d", rowsAffected);
-        ConnectionPoolManager.getInstance().returnConnection(connection);
+
+        getInstance().returnConnection(connection);
         return rowsAffected > 0;
+    }
+
+    public static ResultSet getBook(@NonNull UUID uuid) throws SQLException, InterruptedException {
+        log.atFine().log("Getting book by id: %s", uuid);
+        val connection = getInstance().getConnection();
+
+        val statement = connection.createStatement();
+        val query = String.format(SELECT_BOOK_BY_ID_FORMATTABLE_QUERY, SCHEMA, TABLE, ID, uuid);
+
+        log.atFine().log("Executing query: %s", query);
+        val resultSet = statement.executeQuery(query);
+        log.atFine().log("Query has been successfully executed.");
+
+        getInstance().returnConnection(connection);
+        return resultSet;
+    }
+
+    public static ResultSet getBooks() throws SQLException, InterruptedException {
+        log.atFine().log("Getting all books...");
+        val connection = getInstance().getConnection();
+
+        val statement = connection.createStatement();
+        val query = String.format("select * from %s.%s;", SCHEMA, TABLE);
+
+        log.atFine().log("Executing query: %s", query);
+        val resultSet = statement.executeQuery(query);
+        log.atFine().log("Query has been successfully executed.");
+
+        getInstance().returnConnection(connection);
+        return resultSet;
     }
 }
