220179b65780c18bf406fa56828aa0e5171c82d2
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-28 14:51:34 -0500

    Remove UUID field, add logic to handle get requests.

--- src/main/java/com/github/danielm94/server/handlers/BookHandler.java
+++ src/main/java/com/github/danielm94/server/handlers/BookHandler.java
@@ -1,9 +1,10 @@
 package com.github.danielm94.server.handlers;
 
-import com.github.danielm94.server.requestdata.content.ContentType;
 import com.github.danielm94.server.services.create.CreateBookService;
 import com.github.danielm94.server.services.create.factory.ContentTypeCreateBookServiceFactory;
 import com.github.danielm94.server.services.create.factory.UnsupportedContentTypeException;
+import com.github.danielm94.server.services.read.GetAllBooksService;
+import com.github.danielm94.server.services.read.GetBookByIdService;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import lombok.val;
@@ -11,21 +12,14 @@ import lombok.val;
 import java.io.IOException;
 import java.util.UUID;
 
-import static com.github.danielm94.server.HttpMethod.POST;
-import static com.github.danielm94.server.HttpMethod.getHttpMethodFromStringValue;
-import static com.github.danielm94.server.requestdata.headers.HttpHeader.CONTENT_TYPE;
-import static java.net.HttpURLConnection.HTTP_UNSUPPORTED_TYPE;
+import static com.github.danielm94.server.HttpMethod.*;
+import static com.github.danielm94.server.exchange.Attributes.BOOK_ID;
+import static com.github.danielm94.server.requestdata.content.ContentType.*;
+import static com.github.danielm94.server.requestdata.headers.HttpHeader.*;
+import static java.lang.String.format;
+import static java.net.HttpURLConnection.*;
 
 public class BookHandler implements HttpHandler {
-    private final UUID resourceId;
-
-    public BookHandler() {
-        this.resourceId = null;
-    }
-
-    public BookHandler(UUID resourceId) {
-        this.resourceId = resourceId;
-    }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
@@ -33,18 +27,26 @@ public class BookHandler implements HttpHandler {
         val headers = exchange.getRequestHeaders();
         if (httpMethod == POST) {
             val contentTypeHeaderValue = headers.getFirst(CONTENT_TYPE.toString());
-            val contentType = ContentType.getContentTypeFromString(contentTypeHeaderValue);
+            val contentType = getContentTypeFromString(contentTypeHeaderValue);
             val serviceFactory = new ContentTypeCreateBookServiceFactory();
             CreateBookService createBookService;
             try {
                 createBookService = serviceFactory.getServiceForContentType(contentType);
             } catch (UnsupportedContentTypeException e) {
-                val responseMessage = String.format("Server does not support content type [%s] for http method [%s] at the endpoint [%s].",
+                val responseMessage = format("Server does not support content type [%s] for http method [%s] at the endpoint [%s].",
                         contentType, httpMethod, exchange.getHttpContext().getPath());
                 new SimpleResponseHandler(HTTP_UNSUPPORTED_TYPE, responseMessage).handle(exchange);
                 return;
             }
             createBookService.createBook(exchange);
+        } else if (httpMethod == GET) {
+            val attributes = exchange.getHttpContext().getAttributes();
+            val resourceId = (UUID) exchange.getHttpContext().getAttributes().get(BOOK_ID.toString());
+            if (resourceId == null) new GetAllBooksService().get(exchange);
+            else {
+                attributes.remove(BOOK_ID.toString());
+                new GetBookByIdService(resourceId).get(exchange);
+            }
         }
     }
 }
