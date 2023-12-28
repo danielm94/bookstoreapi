6050ed8d8fba6d94ae96acb4eaca040cc3f78003
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-28 03:20:35 -0500

    Refactor ContentType enum to include lookup enum value using string.

--- src/main/java/com/github/danielm94/server/requestdata/content/ContentType.java
+++ src/main/java/com/github/danielm94/server/requestdata/content/ContentType.java
@@ -1,17 +1,37 @@
 package com.github.danielm94.server.requestdata.content;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.val;
 
-@AllArgsConstructor
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
+@AllArgsConstructor
 public enum ContentType {
     APPLICATION_JSON("application/json");
+    @Getter(AccessLevel.NONE)
+    private static final Map<String, ContentType> lookupMap;
+
 
     private final String mimeType;
+
+
+    static {
+        lookupMap = new HashMap<>();
+        for (val value : values()) {
+            lookupMap.put(value.getMimeType(), value);
+        }
+    }
 
     @Override
     public String toString() {
         return mimeType;
+    }
+
+    public static ContentType getContentTypeFromString(String mimeType) {
+        return lookupMap.get(mimeType);
     }
 }
