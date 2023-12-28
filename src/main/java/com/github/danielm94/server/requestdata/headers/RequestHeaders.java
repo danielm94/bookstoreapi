dc5342e96460f0e48814913a0bd56ad122969325
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-27 23:16:35 -0500

    Moved RequestHeaders enum to requestdata.headers package, use lombok toString annotation.

--- src/main/java/com/github/danielm94/server/requestdata/headers/RequestHeaders.java
+++ src/main/java/com/github/danielm94/server/requestdata/headers/RequestHeaders.java
@@ -1,18 +1,15 @@
-package com.github.danielm94.server.util;
+package com.github.danielm94.server.requestdata.headers;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@ToString(of = "headerKey")
 public enum RequestHeaders {
     CONTENT_LENGTH("Content-Length"),
     CONTENT_TYPE("Content-Type");
 
     private final String headerKey;
-
-    @Override
-    public String toString() {
-        return headerKey;
-    }
 }
