e02b19cf5f31e8634a83385bd58d4ef67d544422
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-29 06:29:29 -0500

    Add utility method to SimpleResponseHandler

--- src/main/java/com/github/danielm94/server/handlers/SimpleResponseHandler.java
+++ src/main/java/com/github/danielm94/server/handlers/SimpleResponseHandler.java
@@ -4,6 +4,7 @@ import com.github.danielm94.server.response.ResponseDispatcher;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
 import lombok.extern.flogger.Flogger;
 
 import java.io.IOException;
@@ -14,11 +15,25 @@ public class SimpleResponseHandler implements HttpHandler {
     private final int statusCode;
     private final String body;
 
-    @Override
-    public void handle(HttpExchange exchange) throws IOException {
+    private static void sendResponseInternal(@NonNull HttpExchange exchange, @NonNull Integer statusCode, String body) throws IOException {
         ResponseDispatcher.createResponse(exchange)
                           .setResponseCode(statusCode)
                           .setBody(body)
                           .sendResponse();
+    }
+
+    public static void sendResponse(@NonNull HttpExchange exchange, @NonNull Integer statusCode, String body) {
+        try {
+            sendResponseInternal(exchange, statusCode, body);
+        } catch (IOException e) {
+            log.atSevere()
+               .withCause(e)
+               .log("Server failed to send response to the client.\nResponse Status: %d\nResponse Message: %s", statusCode, body);
+        }
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        sendResponseInternal(exchange, statusCode, body);
     }
 }
