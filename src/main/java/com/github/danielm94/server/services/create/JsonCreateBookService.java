6f7ae58d5f140d3e4dc8b62060931813af938f44
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-29 08:43:07 -0500

    Use static sendResponse method instead of instantiation, add logic to determine if book was created or not.

--- src/main/java/com/github/danielm94/server/services/create/JsonCreateBookService.java
+++ src/main/java/com/github/danielm94/server/services/create/JsonCreateBookService.java
@@ -2,9 +2,7 @@ package com.github.danielm94.server.services.create;
 
 import com.github.danielm94.database.repository.BookRepository;
 import com.github.danielm94.server.domain.book.BookDTO;
-import com.github.danielm94.server.domain.book.mappers.BookMapper;
 import com.github.danielm94.server.domain.book.mappers.JsonBookDTOMapper;
-import com.github.danielm94.server.handlers.SimpleResponseHandler;
 import com.sun.net.httpserver.HttpExchange;
 import lombok.NonNull;
 import lombok.extern.flogger.Flogger;
@@ -13,6 +11,8 @@ import lombok.val;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import static com.github.danielm94.server.domain.book.mappers.BookMapper.*;
+import static com.github.danielm94.server.handlers.SimpleResponseHandler.sendResponse;
 import static java.net.HttpURLConnection.*;
 
 @Flogger
@@ -26,38 +26,29 @@ public class JsonCreateBookService implements CreateBookService {
         try {
             bookDTO = dtoMapper.parseRequestBodyToBookDTO(exchange.getRequestBody());
         } catch (IOException e) {
-            sendResponseToClient(exchange, HTTP_INTERNAL_ERROR, "Server failed to parse request body into a book suitable for creation operations.");
+            sendResponse(exchange, HTTP_INTERNAL_ERROR, "Server failed to parse request body into a book suitable for creation operations.");
             return;
         }
 
-        val book = BookMapper.mapFromDTO(bookDTO);
+        val book = mapFromDTO(bookDTO);
         boolean bookWasCreated;
         try {
-            bookWasCreated = BookRepository.createBook(book);
+            val rowsAffected = BookRepository.createBook(book);
+            bookWasCreated = rowsAffected > 0;
         } catch (SQLException e) {
-            sendResponseToClient(exchange, HTTP_INTERNAL_ERROR,
+            sendResponse(exchange, HTTP_INTERNAL_ERROR,
                     "Server failed to create a new book in the database as an error occurred while performing database operations.");
             return;
         } catch (InterruptedException e) {
-            sendResponseToClient(exchange, HTTP_INTERNAL_ERROR,
+            sendResponse(exchange, HTTP_INTERNAL_ERROR,
                     "Server failed to create a new book in the database as an error occurred when managing database connections.");
             return;
         }
 
         if (bookWasCreated) {
-            sendResponseToClient(exchange, HTTP_CREATED, null);
+            sendResponse(exchange, HTTP_CREATED, null);
         } else {
-            sendResponseToClient(exchange, HTTP_INTERNAL_ERROR, "Failed to create book resource in database.");
-        }
-    }
-
-    private void sendResponseToClient(HttpExchange exchange, int responseStatus, String message) {
-        try {
-            new SimpleResponseHandler(responseStatus, message).handle(exchange);
-        } catch (IOException e) {
-            log.atSevere()
-               .withCause(e)
-               .log("Server failed to send response to the client.\nResponse Status: %d\nResponse Message: %s", responseStatus, message);
+            sendResponse(exchange, HTTP_INTERNAL_ERROR, "Failed to create book resource in database.");
         }
     }
 }
