12a26c7fc499875f652700e5d10b481e2d809667
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-29 06:27:21 -0500

    Add UUID param to PutBookService update book method

--- src/main/java/com/github/danielm94/server/services/update/PutBookService.java
+++ src/main/java/com/github/danielm94/server/services/update/PutBookService.java
@@ -3,6 +3,8 @@ package com.github.danielm94.server.services.update;
 import com.sun.net.httpserver.HttpExchange;
 import lombok.NonNull;
 
+import java.util.UUID;
+
 public interface PutBookService {
-    void updateBook(@NonNull HttpExchange exchange);
+    void updateBook(@NonNull HttpExchange exchange, @NonNull UUID uuid);
 }
