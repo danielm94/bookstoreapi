2bc33c1495368e5617e962bba58fd497a17cc48f
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-30 08:01:35 -0500

    Remove unnecessary field.

--- src/main/java/com/github/danielm94/server/handlers/book/httpmethod/PostBookHandler.java
+++ src/main/java/com/github/danielm94/server/handlers/book/httpmethod/PostBookHandler.java
@@ -1,7 +1,6 @@
 package com.github.danielm94.server.handlers.book.httpmethod;
 
 import com.github.danielm94.server.requestdata.content.UnsupportedContentTypeException;
-import com.github.danielm94.server.requestdata.method.HttpMethod;
 import com.github.danielm94.server.services.create.CreateBookService;
 import com.github.danielm94.server.services.create.factory.DefaultCreateBookServiceFactory;
 import com.sun.net.httpserver.HttpExchange;
@@ -11,13 +10,13 @@ import lombok.val;
 import static com.github.danielm94.server.handlers.SimpleResponseHandler.sendResponse;
 import static com.github.danielm94.server.requestdata.content.ContentType.*;
 import static com.github.danielm94.server.requestdata.headers.HttpHeader.*;
+import static com.github.danielm94.server.requestdata.method.HttpMethod.POST;
 import static com.github.danielm94.server.requestdata.validation.RequestDataValidator.hasHeader;
 import static com.github.danielm94.server.requestdata.validation.RequestDataValidator.hasRequestBody;
 import static java.lang.String.format;
 import static java.net.HttpURLConnection.*;
 
 public class PostBookHandler implements HttpMethodBookHandler {
-    public static final HttpMethod HTTP_METHOD = HttpMethod.POST;
 
     @Override
     public void handle(@NonNull HttpExchange exchange) {
@@ -40,7 +39,7 @@ public class PostBookHandler implements HttpMethodBookHandler {
             createBookService = serviceFactory.getServiceForContentType(contentType);
         } catch (UnsupportedContentTypeException e) {
             val responseMessage = format("Server does not support content type [%s] for http method [%s] at the endpoint [%s].",
-                    contentType, HTTP_METHOD, exchange.getHttpContext().getPath());
+                    contentType, POST, exchange.getHttpContext().getPath());
             sendResponse(exchange, HTTP_UNSUPPORTED_TYPE, responseMessage);
             return;
         }
