25c982320d0af0c9cf9b5615f7693e720a7a1978
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-30 08:01:06 -0500

    Add UUID parameter to patch abstract method.

--- src/main/java/com/github/danielm94/server/services/update/PatchBookService.java
+++ src/main/java/com/github/danielm94/server/services/update/PatchBookService.java
@@ -3,7 +3,9 @@ package com.github.danielm94.server.services.update;
 import com.sun.net.httpserver.HttpExchange;
 import lombok.NonNull;
 
+import java.util.UUID;
+
 public interface PatchBookService {
 
-    void patch(@NonNull HttpExchange exchange);
+    void patch(@NonNull HttpExchange exchange, @NonNull UUID uuid);
 }
