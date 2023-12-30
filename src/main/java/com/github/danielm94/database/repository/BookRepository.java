5833da2ec12b98cb7bcf44175c143a86ce34d1db
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2023-12-30 08:20:05 -0500

    Add method to handle patching books, clean up code.

--- src/main/java/com/github/danielm94/database/repository/BookRepository.java
+++ src/main/java/com/github/danielm94/database/repository/BookRepository.java
@@ -7,8 +7,10 @@ import lombok.NonNull;
 import lombok.extern.flogger.Flogger;
 import lombok.val;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.UUID;
 
 import static com.github.danielm94.ConnectionPoolManager.getInstance;
@@ -33,6 +35,7 @@ public class BookRepository {
 
     public static int createBook(@NonNull Book book) throws SQLException, InterruptedException {
         log.atFine().log("Creating book in %s.%s table for the book:\n%s", SCHEMA, TABLE, book);
+
         val query = format(CREATE_NEW_BOOK_FORMATTABLE_QUERY,
                 SCHEMA, TABLE,
                 ID, BOOK_NAME, AUTHOR, ISBN, PRICE, DATE_ADDED, DATE_UPDATED,
@@ -94,12 +97,7 @@ public class BookRepository {
     }
 
     public static int updateBook(@NonNull Book book) throws MissingBookIDException, SQLException, InterruptedException {
-        val id = book.getId();
-        if (id == null) {
-            val message = format("Expected the book to have an ID, but it was null. The book in question:\n%s", book);
-            log.atWarning().log(message);
-            throw new MissingBookIDException(message);
-        }
+        val id = getAndValidateBookId(book);
 
         log.atFine().log("Updating book with ID of [%s] with new book:\n%s", id, book);
 
@@ -144,5 +142,74 @@ public class BookRepository {
 
         log.atFine().log("Successfully executed query to delete book. Rows affected: %n", rowsAffected);
         return rowsAffected;
+    }
+
+    public static int patchBook(@NonNull Book book) throws SQLException, InterruptedException, MissingBookIDException {
+        val id = getAndValidateBookId(book);
+
+        log.atFine().log("Patching book with ID of [%s] with new book:\n%s", id, book);
+        val columnValueList = new ArrayList<String>();
+
+        val query = createPatchQuery(book, columnValueList);
+        if (columnValueList.isEmpty()) return 0;
+
+        val connection = getInstance().getConnection();
+        val statement = connection.prepareStatement(query);
+        plugInValuesIntoPatchQuery(columnValueList, statement);
+        val rowsAffected = statement.executeUpdate();
+        getInstance().returnConnection(connection);
+
+        return rowsAffected;
+    }
+
+    private static UUID getAndValidateBookId(Book book) throws MissingBookIDException {
+        val id = book.getId();
+        if (id == null) {
+            val message = format("Expected the book to have an ID, but it was null. The book in question:\n%s", book);
+            log.atWarning().log(message);
+            throw new MissingBookIDException(message);
+        }
+        return id;
+    }
+
+    private static void plugInValuesIntoPatchQuery(ArrayList<String> columnValueList, PreparedStatement statement) throws SQLException {
+        for (var i = 0; i < columnValueList.size(); i++) {
+            val columnValue = columnValueList.get(i);
+            statement.setString(i + 1, columnValue);
+        }
+    }
+
+    private static String createPatchQuery(Book book, ArrayList<String> columnValueList) {
+        val queryBuilder = new StringBuilder();
+        queryBuilder.append("update ").append(SCHEMA).append('.').append(TABLE).append(" SET");
+
+        if (book.getBookName() != null) {
+            queryBuilder.append(" ").append(BOOK_NAME).append(" = ?,");
+            columnValueList.add(book.getBookName());
+        }
+        if (book.getAuthor() != null) {
+            queryBuilder.append(" ").append(AUTHOR).append(" = ?,");
+            columnValueList.add(book.getAuthor());
+        }
+        if (book.getIsbn() != null) {
+            queryBuilder.append(" ").append(ISBN).append(" = ?,");
+            columnValueList.add(book.getIsbn());
+        }
+        if (book.getPrice() != null) {
+            queryBuilder.append(" ").append(PRICE).append(" = ?,");
+            columnValueList.add(book.getPrice().toString());
+        }
+        if (book.getDateUpdated() != null) {
+            queryBuilder.append(" ").append(DATE_UPDATED).append(" = ?,");
+            columnValueList.add(book.getDateUpdated().toString());
+        }
+        if (book.getId() != null) {
+            columnValueList.add(book.getId().toString());
+        }
+
+        queryBuilder.deleteCharAt(queryBuilder.length() - 1)
+                    .append(" where ").append(ID).append(" = ?;");
+
+        return queryBuilder.toString();
     }
 }
