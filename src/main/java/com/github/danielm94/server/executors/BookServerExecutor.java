10be9bd50d31068bdfbc718b426b674a3e98b7e6
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2024-01-02 16:47:42 -0500

    Add non null annotation to parameter.

--- src/main/java/com/github/danielm94/server/executors/BookServerExecutor.java
+++ src/main/java/com/github/danielm94/server/executors/BookServerExecutor.java
@@ -1,10 +1,12 @@
 package com.github.danielm94.server.executors;
 
+import lombok.NonNull;
+
 import java.util.concurrent.Executor;
 
 public class BookServerExecutor implements Executor {
     @Override
-    public void execute(Runnable command) {
+    public void execute(@NonNull Runnable command) {
         command.run();
     }
 }
