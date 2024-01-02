0e3680e26bf3f6632caab93297dad0d7795f2cd0
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2024-01-02 17:27:32 -0500

    Add nonnull annotation to ExceptionHandler fields.

--- src/main/java/com/github/danielm94/server/handlers/ExceptionHandler.java
+++ src/main/java/com/github/danielm94/server/handlers/ExceptionHandler.java
@@ -3,12 +3,15 @@ package com.github.danielm94.server.handlers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
 import lombok.extern.flogger.Flogger;
 
 @Flogger
 @AllArgsConstructor
 public class ExceptionHandler implements HttpHandler {
+    @NonNull
     private final String logMessage;
+    @NonNull
     private final Throwable cause;
 
     @Override
