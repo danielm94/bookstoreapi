c4cef0f313554d8f37c948646075276274de3928
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2024-01-04 08:22:34 -0500

    Add Accept header to HttpHeader enum.

--- src/main/java/com/github/danielm94/server/requestdata/headers/HttpHeader.java
+++ src/main/java/com/github/danielm94/server/requestdata/headers/HttpHeader.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum HttpHeader {
+    ACCEPT("Accept"),
     CONTENT_LENGTH("Content-Length"),
     CONTENT_TYPE("Content-Type"),
     LOCATION("Location");
