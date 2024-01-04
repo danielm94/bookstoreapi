37267b1feb926f42aae6e5a71e53fdb8c0bf2576
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2024-01-04 08:12:15 -0500

    Replace any with throwable because it's free baby.

--- src/test/java/com/github/danielm94/server/handlers/ExceptionHandlerTest.java
+++ src/test/java/com/github/danielm94/server/handlers/ExceptionHandlerTest.java
@@ -4,14 +4,13 @@ package com.github.danielm94.server.handlers;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
 
 @SuppressWarnings("DataFlowIssue")
 class ExceptionHandlerTest {
 
     @Test
     void nullPointerExceptionShouldBeThrownWhenPassingNullMessage() {
-        assertThatThrownBy(() -> new ExceptionHandler(null, any(Throwable.class)))
+        assertThatThrownBy(() -> new ExceptionHandler(null, new Throwable()))
                 .as("NullPointerException should be thrown if a null log message is passed.")
                 .isInstanceOf(NullPointerException.class);
     }
