8ed0869a91e18aa4210061ef4af00181ddd4cfb8
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2024-01-04 10:03:25 -0500

    Make unsupported http method status code and response message publicly accessible constants.

--- src/main/java/com/github/danielm94/server/handlers/book/BookHandler.java
+++ src/main/java/com/github/danielm94/server/handlers/book/BookHandler.java
@@ -2,6 +2,7 @@ package com.github.danielm94.server.handlers.book;
 
 import com.github.danielm94.server.handlers.book.httpmethod.HttpMethodBookHandler;
 import com.github.danielm94.server.handlers.book.httpmethod.factory.HttpMethodBookHandlerFactory;
+import com.github.danielm94.server.requestdata.method.HttpMethod;
 import com.github.danielm94.server.requestdata.method.UnsupportedHttpMethodException;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -17,8 +18,13 @@ import static java.net.HttpURLConnection.HTTP_BAD_METHOD;
 @Flogger
 @AllArgsConstructor
 public class BookHandler implements HttpHandler {
+    public static final int UNSUPPORTED_HTTP_METHOD_STATUS_CODE = HTTP_BAD_METHOD;
     @NonNull
     private final HttpMethodBookHandlerFactory factory;
+
+    public static String getUnsupportedHTTPMethodMessage(HttpMethod httpMethod) {
+        return String.format("Server does not support the request method %s for this endpoint.", httpMethod);
+    }
 
     @Override
     public void handle(@NonNull HttpExchange exchange) {
@@ -27,8 +33,7 @@ public class BookHandler implements HttpHandler {
         try {
             handler = factory.getHandler(httpMethod);
         } catch (UnsupportedHttpMethodException e) {
-            val message = String.format("Server does not support the request method %s for this endpoint.", httpMethod);
-            sendResponse(exchange, HTTP_BAD_METHOD, message);
+            sendResponse(exchange, UNSUPPORTED_HTTP_METHOD_STATUS_CODE, getUnsupportedHTTPMethodMessage(httpMethod));
             return;
         }
         handler.handle(exchange);
