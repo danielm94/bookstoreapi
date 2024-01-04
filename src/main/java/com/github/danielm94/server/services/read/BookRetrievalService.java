62c77ecd33269e4a28e32135ead70d17e76801da
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2024-01-04 08:23:45 -0500

    Merge GET book services into one service. BookSerializer is no longer a field and instead is a method parameter.

--- src/main/java/com/github/danielm94/server/services/read/BookRetrievalService.java
+++ src/main/java/com/github/danielm94/server/services/read/BookRetrievalService.java
@@ -4,7 +4,6 @@ import com.github.danielm94.server.domain.book.Book;
 import com.github.danielm94.server.domain.book.serializer.BookSerializationException;
 import com.github.danielm94.server.domain.book.serializer.BookSerializer;
 import com.sun.net.httpserver.HttpExchange;
-import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.extern.flogger.Flogger;
 import lombok.val;
@@ -15,25 +14,71 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.UUID;
 
-import static com.github.danielm94.database.repository.BookRepository.*;
-import static com.github.danielm94.database.schemas.DatabaseSchemas.*;
-import static com.github.danielm94.database.schemas.bookstoreapi.DatabaseTables.*;
-import static com.github.danielm94.server.domain.book.mappers.BookMapper.*;
+import static com.github.danielm94.database.repository.BookRepository.getBook;
+import static com.github.danielm94.database.repository.BookRepository.getBooks;
+import static com.github.danielm94.database.schemas.DatabaseSchemas.BOOKSTOREAPI;
+import static com.github.danielm94.database.schemas.bookstoreapi.DatabaseTables.BOOKS;
+import static com.github.danielm94.server.domain.book.mappers.BookMapper.mapFromResultSet;
 import static com.github.danielm94.server.handlers.SimpleResponseHandler.sendResponse;
-import static com.github.danielm94.server.requestdata.content.ContentType.*;
-import static com.github.danielm94.server.requestdata.headers.HttpHeader.*;
+import static com.github.danielm94.server.requestdata.content.ContentType.APPLICATION_JSON;
+import static com.github.danielm94.server.requestdata.headers.HttpHeader.CONTENT_TYPE;
 import static com.github.danielm94.server.response.ResponseDispatcher.createResponse;
 import static java.net.HttpURLConnection.*;
 import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 
+
 @Flogger
-@AllArgsConstructor
-public class GetBookByIdService implements GetBookService {
-    private final BookSerializer bookSerializer;
-    private final UUID uuid;
+public class BookRetrievalService implements GetBookService {
 
     @Override
-    public void get(@NonNull HttpExchange exchange) {
+    public void getAll(@NonNull HttpExchange exchange, @NonNull BookSerializer serializer) {
+        ResultSet resultSet;
+        try {
+            resultSet = getBooks();
+        } catch (SQLException e) {
+            sendResponse(exchange, HTTP_INTERNAL_ERROR, "An error occurred while getting books from the database.");
+            return;
+        } catch (InterruptedException e) {
+            sendResponse(exchange, HTTP_INTERNAL_ERROR, "An error occurred while managing connections to the database.");
+            return;
+        }
+
+        List<Book> books;
+        try {
+            books = mapFromResultSet(resultSet, ISO_LOCAL_DATE_TIME);
+        } catch (SQLException e) {
+            sendResponse(exchange, HTTP_INTERNAL_ERROR, "An error occurred while mapping data from the database.");
+            return;
+        }
+
+        if (books.isEmpty()) {
+            sendResponse(exchange, HTTP_NOT_FOUND, "Books table is empty.");
+            return;
+        }
+
+        String body;
+        try {
+            body = serializer.serializeBooks(books);
+        } catch (BookSerializationException e) {
+            sendResponse(exchange, HTTP_INTERNAL_ERROR, "Server failed to create response body.");
+            return;
+        }
+
+        try {
+            createResponse(exchange)
+                    .setResponseCode(HTTP_OK)
+                    .addHeader(CONTENT_TYPE.toString(), APPLICATION_JSON.toString())
+                    .setBody(body)
+                    .sendResponse();
+        } catch (IOException e) {
+            log.atSevere()
+               .withCause(e)
+               .log("Failed to send response to server after getting all books.");
+        }
+    }
+
+    @Override
+    public void getAllById(@NonNull HttpExchange exchange, @NonNull BookSerializer serializer, @NonNull UUID uuid) {
         ResultSet resultSet;
         try {
             resultSet = getBook(uuid);
@@ -64,7 +109,7 @@ public class GetBookByIdService implements GetBookService {
         val book = books.getFirst();
         String body;
         try {
-            body = bookSerializer.serializeBook(book);
+            body = serializer.serializeBook(book);
         } catch (BookSerializationException e) {
             sendResponse(exchange, HTTP_INTERNAL_ERROR, "Server failed to create response body.");
             return;
--- src/main/java/com/github/danielm94/server/services/read/GetBookService.java
+++ src/main/java/com/github/danielm94/server/services/read/GetBookService.java
@@ -1,8 +1,13 @@
 package com.github.danielm94.server.services.read;
 
+import com.github.danielm94.server.domain.book.serializer.BookSerializer;
 import com.sun.net.httpserver.HttpExchange;
 import lombok.NonNull;
 
+import java.util.UUID;
+
 public interface GetBookService {
-    void get(@NonNull HttpExchange exchange);
+    void getAll(@NonNull HttpExchange exchange, @NonNull BookSerializer bookSerializer);
+
+    void getAllById(@NonNull HttpExchange exchange, @NonNull BookSerializer bookSerializer, @NonNull UUID uuid);
 }
