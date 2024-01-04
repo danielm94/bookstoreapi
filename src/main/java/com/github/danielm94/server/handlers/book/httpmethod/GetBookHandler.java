60430f2dec4717ff1a50c2b7acf9aff5ec615da8
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2024-01-04 08:25:05 -0500

    Add dependency injection support for GetBookService, book serializer is no longer hard coded and instead changes depending on accept header.

--- src/main/java/com/github/danielm94/server/handlers/book/httpmethod/GetBookHandler.java
+++ src/main/java/com/github/danielm94/server/handlers/book/httpmethod/GetBookHandler.java
@@ -1,35 +1,59 @@
 package com.github.danielm94.server.handlers.book.httpmethod;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.github.danielm94.server.domain.book.serializer.JsonBookSerializer;
-import com.github.danielm94.server.services.read.GetAllBooksService;
-import com.github.danielm94.server.services.read.GetBookByIdService;
+import com.github.danielm94.server.domain.book.serializer.BookSerializer;
+import com.github.danielm94.server.domain.book.serializer.factory.DefaultBookSerializerFactory;
+import com.github.danielm94.server.requestdata.content.ContentType;
+import com.github.danielm94.server.requestdata.content.UnsupportedContentTypeException;
+import com.github.danielm94.server.services.read.GetBookService;
 import com.sun.net.httpserver.HttpExchange;
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.val;
 
 import java.util.UUID;
 
-import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
 import static com.github.danielm94.server.exchange.Attributes.BOOK_ID;
+import static com.github.danielm94.server.handlers.SimpleResponseHandler.sendResponse;
+import static com.github.danielm94.server.requestdata.content.ContentType.getContentTypeFromString;
+import static com.github.danielm94.server.requestdata.headers.HttpHeader.ACCEPT;
+import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
+import static java.net.HttpURLConnection.HTTP_NOT_ACCEPTABLE;
 
+@AllArgsConstructor
 public class GetBookHandler implements HttpMethodBookHandler {
+    @NonNull
+    private GetBookService bookService;
+
     @Override
     public void handle(@NonNull HttpExchange exchange) {
-        val mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.disable(WRITE_DATES_AS_TIMESTAMPS);
-
-        val bookSerializer = new JsonBookSerializer(mapper);
-
         val attributes = exchange.getHttpContext().getAttributes();
         val resourceId = (UUID) attributes.get(BOOK_ID.toString());
 
-        if (resourceId == null) new GetAllBooksService(bookSerializer).get(exchange);
-        else {
+        val headers = exchange.getRequestHeaders();
+        val accept = headers.get(ACCEPT.toString()).getFirst();
+
+        ContentType contentType;
+        try {
+            contentType = getContentTypeFromString(accept);
+        } catch (UnsupportedContentTypeException e) {
+            sendResponse(exchange, HTTP_BAD_REQUEST, accept + " is not a supported content type.");
+            return;
+        }
+
+        val factory = new DefaultBookSerializerFactory();
+        BookSerializer serializer;
+        try {
+            serializer = factory.getSerializer(contentType);
+        } catch (UnsupportedContentTypeException e) {
+            sendResponse(exchange, HTTP_NOT_ACCEPTABLE, "Server does not support formatting the books into " + contentType);
+            return;
+        }
+
+        if (resourceId == null) {
+            bookService.getAll(exchange, serializer);
+        } else {
             attributes.remove(BOOK_ID.toString());
-            new GetBookByIdService(bookSerializer, resourceId).get(exchange);
+            bookService.getAllById(exchange, serializer, resourceId);
         }
     }
 }
