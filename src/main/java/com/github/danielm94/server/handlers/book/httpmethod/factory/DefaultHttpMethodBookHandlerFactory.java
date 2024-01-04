7abbd8584afbf93326fc3f960aeada1a423e35fc
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2024-01-04 08:25:28 -0500

    Inject GetBookService into GetBookHandler in factory.

--- src/main/java/com/github/danielm94/server/handlers/book/httpmethod/factory/DefaultHttpMethodBookHandlerFactory.java
+++ src/main/java/com/github/danielm94/server/handlers/book/httpmethod/factory/DefaultHttpMethodBookHandlerFactory.java
@@ -4,13 +4,14 @@ import com.github.danielm94.server.handlers.book.httpmethod.*;
 import com.github.danielm94.server.requestdata.method.HttpMethod;
 import com.github.danielm94.server.requestdata.method.UnsupportedHttpMethodException;
 import com.github.danielm94.server.services.delete.BookRemovalService;
+import com.github.danielm94.server.services.read.BookRetrievalService;
 import lombok.NonNull;
 
 public class DefaultHttpMethodBookHandlerFactory implements HttpMethodBookHandlerFactory {
     @Override
     public HttpMethodBookHandler getHandler(@NonNull HttpMethod method) throws UnsupportedHttpMethodException {
         return switch (method) {
-            case GET, HEAD -> new GetBookHandler();
+            case GET, HEAD -> new GetBookHandler(new BookRetrievalService());
             case PUT -> new PutBookHandler();
             case POST -> new PostBookHandler();
             case DELETE -> new DeleteBookHandler(new BookRemovalService());
