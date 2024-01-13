1936abcc32797cf9eb7060fe662ad0d4aa4ce9ac
Daniel Martins <24377240+danielm94@users.noreply.github.com>
2024-01-13 06:15:55 -0500

    Initial commit of OutputStreamRetrievalException

---
+++ src/main/java/com/github/danielm94/server/parsers/OutputStreamRetrievalException.java
@@ -0,0 +1,7 @@
+package com.github.danielm94.server.parsers;
+
+public class OutputStreamRetrievalException extends Exception {
+    public OutputStreamRetrievalException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
